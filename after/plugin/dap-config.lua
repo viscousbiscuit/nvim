f5a643f3e24af70d6039a1da2268c02f041009cb
Timothy von Hollen <timothyvonhollen@gmail.com>
2023-07-25 21:26:36 -0700

    chore: added additional dap configs

--- after/plugin/dap-config.lua
+++ after/plugin/dap-config.lua
@@ -22,15 +22,22 @@ dap.configurations.php = {
 }
 
 
+require("dapui").setup()
+
+vim.fn.sign_define('DapBreakpoint',{ text ='󱠇', texthl ='', linehl ='', numhl =''})
+vim.fn.sign_define('DapStopped',{ text ='', texthl ='', linehl ='', numhl =''})
 
 vim.api.nvim_set_keymap('n', '<F8>', [[:lua require"dap".toggle_breakpoint()<CR>]], { noremap = true })
 vim.api.nvim_set_keymap('n', '<F10>', [[:lua require"dap".step_over()<CR>]], { noremap = true })
 vim.api.nvim_set_keymap('n', '<F12>', [[:lua require"dap.ui.widgets".hover()<CR>]], { noremap = true })
 vim.api.nvim_set_keymap('n', '<F11>', [[:lua require"dap".step_into()<CR>]], { noremap = true})
 vim.api.nvim_set_keymap('n', '<S-F11>', [[:lua require"dap".step_out()<CR>]], { noremap = true})
-
-
+vim.api.nvim_set_keymap('n', '<F6>', [[:DapTerminate<CR> :lua require('dapui').close()<CR>]], { noremap = true})
 vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+
+vim.api.nvim_set_keymap('n', '<Leader>de', [[:lua require("dapui").eval()<CR>]], { noremap = true})
+
+
     vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
       require('dap.ui.widgets').hover()
     end)
@@ -47,19 +54,26 @@ vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
     end)
 
 
-require("dapui").setup()
+
+
+
 
 local dap, dapui = require("dap"), require("dapui")
-dap.listeners.after.event_initialized["dapui_config"] = function()
-  dapui.open()
+dap.listeners.before.event_terminated["dap"] = function()
+    print('dap before.event_terminated')
+  -- dapui.close()
 end
-dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
+
+
+dap.listeners.after.event_initialized['dap'] = function()
+    print('dap after.event_initialized')
+   dapui.open()
 end
-dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
+
+dap.listeners.before.event_exited["dap"] = function()
+    print('dap before.event_exited')
+  -- dapui.close()
 end
-dap.listeners.after.event_terminated["dapui_config"] = function()
-  dapui.close()
-end
+
+
 
