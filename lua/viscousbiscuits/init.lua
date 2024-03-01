78aaad776cc5080bcd25b7e5d96567833b938763
Timothy von Hollen <timothyvonhollen@gmail.com>
2024-02-29 17:04:09 -0800

    feat(lazy): converted to lazy

--- lua/viscousbiscuits/init.lua
+++ lua/viscousbiscuits/init.lua
@@ -1,71 +1,53 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+    { 'nvimdev/dashboard-nvim', lazy = false },
+    { 'nvim-tree/nvim-web-devicons', lazy = true },
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
+    { 'wakatime/vim-wakatime' },
+    { 'folke/tokyonight.nvim' },
+    { 'nvim-treesitter/nvim-treesitter', build = ":TSUpdate" },
+    { 'mbbill/undotree', lazy = true },
+    { 'nvim-lua/plenary.nvim' },
+    { 'nvim-telescope/telescope.nvim' },
+    { 'nvim-telescope/telescope-file-browser.nvim', lazy = true },
+    { 'gbrlsnchs/telescope-lsp-handlers.nvim' },
+    { 'nvim-tree/nvim-web-devicons', lazy = true },
+    { 'L3MON4D3/LuaSnip' },
+    { 'lewis6991/gitsigns.nvim' },
+    { 'onsails/lspkind-nvim' },
+    { 'nvim-lualine/lualine.nvim'},
+    { 'neovim/nvim-lspconfig'},
+    { 'jose-elias-alvarez/null-ls.nvim' },       
+    { 'hrsh7th/nvim-cmp'},                       
+    { 'hrsh7th/cmp-nvim-lsp'},               
+    { 'hrsh7th/cmp-buffer'},
+    { 'hrsh7th/cmp-path'},
+    { 'hrsh7th/cmp-cmdline'},                
+    { 'hrsh7th/cmp-nvim-lsp-signature-help'},
+    { 'saadparwaiz1/cmp_luasnip'},
+    { 'theHamsta/nvim-dap-virtual-text' },
+    { 'mfussenegger/nvim-dap', lazy = true },
+    { 'stevearc/aerial.nvim' },
+    { 'mfussenegger/nvim-dap' },
+    { 'rcarriga/nvim-dap-ui' },
+    { 'mxsdev/nvim-dap-vscode-js' }
+}
+
+require("lazy").setup(plugins, opts)
 require('viscousbiscuits.remap')
 
 vim.cmd("set number")
 vim.cmd("set relativenumber")
-
-return require('packer').startup(function()
--- General
-use 'williamboman/mason.nvim'
-use 'williamboman/mason-lspconfig.nvim'
-use 'wbthomason/packer.nvim'
-
--- Startup
- use {
-   'nvimdev/dashboard-nvim',
-   requires = {'nvim-tree/nvim-web-devicons'}
- }
-
--- Time Tracking
-use 'wakatime/vim-wakatime'
-
--- Theme and UI/UX
-use 'folke/tokyonight.nvim' 
-
-use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function()
-        local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-        ts_update()
-    end,
-}
-
-use 'mbbill/undotree' 
-
-use { 'nvim-telescope/telescope.nvim', requires = { {'nvim-lua/plenary.nvim'} }}
-use {
-    "nvim-telescope/telescope-file-browser.nvim",
-    requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
-} 
-use {
-    "gbrlsnchs/telescope-lsp-handlers.nvim",
-    requires = { "nvim-telescope/telescope.nvim"}
-    }
-
-use 'nvim-tree/nvim-web-devicons'
-use 'L3MON4D3/LuaSnip'
-use 'lewis6991/gitsigns.nvim'
-use 'onsails/lspkind-nvim' 
-use { 'nvim-lualine/lualine.nvim', requires = { 'nvim-tree/nvim-web-devicons', opt = true }}
-
--- LSP
-use 'neovim/nvim-lspconfig'
-use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
-use 'hrsh7th/nvim-cmp'
-use 'hrsh7th/cmp-nvim-lsp'
-use 'hrsh7th/cmp-buffer'
-use 'hrsh7th/cmp-path'
-use 'hrsh7th/cmp-cmdline'
-use 'hrsh7th/cmp-nvim-lsp-signature-help';
-use 'saadparwaiz1/cmp_luasnip'
-
--- DAP plugins
-use 'theHamsta/nvim-dap-virtual-text'
-use 'mfussenegger/nvim-dap'
-use 'stevearc/aerial.nvim'
-use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
-use { "mxsdev/nvim-dap-vscode-js", requires = {"mfussenegger/nvim-dap"} }
-
--- Git Plugins
-use 'sindrets/diffview.nvim' 
-
-end)
