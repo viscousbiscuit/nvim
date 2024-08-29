aab4373b1394020ebf20c208b71956686fec47ce
Timothy von Hollen <timothyvonhollen@gmail.com>
2024-08-28 17:27:19 -0700

    chore: updated for wsl

--- lua/viscousbiscuits/init.lua
+++ lua/viscousbiscuits/init.lua
@@ -75,3 +75,18 @@ require("viscousbiscuits.remap")
 
 vim.cmd("set number")
 vim.cmd("set relativenumber")
+
+if vim.fn.has('wsl') == 1 then
+    vim.g.clipboard = {
+        name = 'WslClipboard',
+        copy = {
+            ['+'] = 'clip.exe',
+            ['*'] = 'clip.exe',
+        },
+        paste = {
+            ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        },
+        cache_enabled = 0,
+    }
+end
